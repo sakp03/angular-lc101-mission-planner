f32b7cb8d369f4a9edb20ce94a7a29f5d9128133
Sakura Phansiri <you@example.com>
2021-01-07 13:21:50 -0600

    Adding changes to crew section

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './crew.component.html',
   styleUrls: ['./crew.component.css']
 })
+
 export class CrewComponent implements OnInit {
 
   crew: object[] = [
@@ -15,7 +16,26 @@ export class CrewComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  add(memberName: string, isFirst: boolean) {
+    this.crew.push({name: memberName, firstMission: isFirst});
+  }
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  memberBeingEdited: object = null;
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+  }
+
+  save(name: string, member: object) {
+    member['name'] = name;
+    this.memberBeingEdited = null;
   }
 
 }
